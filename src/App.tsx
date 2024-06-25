e24a95aa52fb3815cce7b9d2dc5800123072e68b
Basmala <basmalazayn46@gmail.com>
2024-06-25 04:06:10 +0300

    add logs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-// App.tsx
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AnalyticsPage from './components/AnalyticsPage';
 import Dashboard from "./components/Dashboard";
 import AuthCallback from "./components/AuthCallback";
-import Login from "./components/Login/Login.tsx"
+import Login from "./components/Login/Login.tsx";
+import DockerLogs from "./components/DockerLogs";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/auth/callback" component={AuthCallback} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/analytics/:containerId" component={AnalyticsPage} />
+        <Route path="/logs/:containerId" component={DockerLogs} />
       </Switch>
     </Router>
   );
--- src/services/AnalyticsService.ts
+++ src/services/AnalyticsService.ts
@@ -15,17 +15,16 @@ const getAnalyticsData = async (containerId: string, interval: string): Promise<
       },
     });
 
-    // Assuming response.data is an array of objects from your backend
+  
     const fetchedData: any[] = response.data || [];
 
-    // Map and filter data
+ 
     const mappedData: AnalyticsData[] = fetchedData.map((entry: any) => ({
       _time: entry._time,
       cpuUsage: entry._field === 'cpuUsage' ? entry._value : undefined,
       memoryUsage: entry._field === 'memoryUsage' ? entry._value : undefined,
     }));
 
-    // Combine data entries with the same timestamp
     const combinedData: { [key: string]: AnalyticsData } = {};
     mappedData.forEach((entry: AnalyticsData) => {
       const key = entry._time;
