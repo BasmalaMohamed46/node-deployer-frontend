e4cf2cd944f80a91a7fbea516af62ba71d6ae173
Belal Abo Ata <belalbelalmohamed1234@gmail.com>
2024-06-30 00:13:44 +0300

    feat: change the profile states

--- src/components/ProfileState.tsx
+++ src/components/ProfileState.tsx
@@ -1,45 +1,62 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
+import { axiosInstance } from '../interceptors/auth.interceptor';
 
 const ProfileState = () => {
-    const userName = "Jhon Doe";
-    const balance = 15;
-    const [loggedIn, setLoggedIn] = useState<boolean>(false);
-    const provider = localStorage.getItem("provider");
-    const accessToken = localStorage.getItem("accessToken");
-
+  const { isLoggedIn, logout } = useAuth();
+  const navigate = useNavigate();
+  function handleLogout() {
+    logout();
+    navigate('/login');
+  }
+  const [username, setUsername] = useState();
+  const [balance, setBalance] = useState();
 
   useEffect(() => {
-    if (provider && accessToken) {
-      setLoggedIn(true);
+    const accessToken: string | null = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      const { id } = jwtDecode(accessToken);
+      axiosInstance.get(`/user/${id}`).then((res) => {
+        const {username, balance} = res.data;
+        setUsername(username);
+        setBalance(balance)
+      }).catch((err) => {
+        console.log(err);
+      })
     }
-  }
-  , [provider, accessToken]);
+  }, [isLoggedIn]);
 
   return (
     <>
-        {
-        loggedIn? (
-            <div className="dropdown me-5 pe-5"><Link className="nav-link" to="#"><span>{userName}</span> <i className="bi bi-chevron-down toggle-dropdown"></i></Link>
-            <ul>
-              <li><Link className="nav-link" to="#">Balance: {balance} $</Link></li>
-              <hr />
-              <li><Link className="nav-link" to="#">Profile</Link></li>
-              <hr />
-              <li><Link className="nav-link" to="#">Logout</Link></li>
-        
-            </ul>
-          </div>
-        ) : (
-          <Link className="btn-getstarted nav-link" to="/login">
-            Login
+      {isLoggedIn ? (
+        <div className="dropdown me-5 pe-5">
+          <Link className="nav-link" to="#">
+            <span>{username}</span> <i className="bi bi-chevron-down toggle-dropdown"></i>
           </Link>
-        )
-    }
+          <ul>
+            <li>
+              <Link className="nav-link" to="#">
+                Balance: {balance} $
+              </Link>
+            </li>
+            <hr />
+            <li>
+              <button className="nav-link px-4 py-2" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <Link className="btn-getstarted nav-link" to="/login">
+          Login
+        </Link>
+      )}
     </>
   );
-}
+};
 
 export default ProfileState;
-
-
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,10 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 
 export type AuthContextType = {
-  isLoggedIn: boolean,
-  login: () => void,
-  logout: () => void
-}
+  isLoggedIn: boolean;
+  login: (provider: string, accesssToken: string) => void;
+  logout: () => void;
+};
 const AuthContext = createContext<AuthContextType>({
   isLoggedIn: false,
   login: () => '',
@@ -15,12 +15,19 @@ export const useAuth = () => {
   return useContext(AuthContext);
 };
 
-
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const login = () => setIsLoggedIn(true);
-  const logout = () => setIsLoggedIn(false);
+  const login = (provider: string, accesssToken: string) => {
+    localStorage.setItem('provider', provider);
+    localStorage.setItem('accessToken', accesssToken);
+    setIsLoggedIn(true);
+  };
+  const logout = () => {
+    localStorage.removeItem('provider');
+    localStorage.removeItem('accessToken');
+    setIsLoggedIn(false);
+  };
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>{children}</AuthContext.Provider>
