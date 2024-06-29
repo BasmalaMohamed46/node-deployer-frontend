3c7cd0a6d796f74978c9850256c59d6b540d4669
Ziad Mohamed Elganzory <ziadmohamed770@outlook.com>
2024-06-29 23:36:19 +0300

    404 Page Handling

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import Aos from "aos";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default HomePage;
